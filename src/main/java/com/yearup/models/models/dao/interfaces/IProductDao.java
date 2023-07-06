e1a9a680ecba53efdf374f0e7b5c5aed9d0673fd
Lily3214 <yeus1995@gmail.com>
2023-07-06 16:07:47 -0400

    upload to github

---
+++ src/main/java/com/yearup/models/models/dao/interfaces/IProductDao.java
@@ -0,0 +1,17 @@
+package com.yearup.models.dao.interfaces;
+
+import com.yearup.models.models.Product;
+
+import java.util.List;
+
+public interface IProductDao {
+    void add(Product product);
+
+    List<Product> getAll();
+
+    Product getProductById(int productId);
+
+    void update(Product product);
+
+    void delete(Product product);
+}
